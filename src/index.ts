5107e24d015446a67a18252f856cab6042338ce6
Juan Moreno <morjua19@gmail.com>
2021-08-24 23:21:43 -0400

    [UPD] se agrego validacion por canal, sacar a una persona y borrar mensajes

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,36 @@ client.on('ready', () => {
     console.log('Bot is ready!');
 });
 
+const isChannelValid = ({id} : {id: string}) => {
+    return id === process.env.CHANNEL_ID_VALID;
+}
+
 client.on('message', async (message: Message) => {
     if (message.content.startsWith(`${config.prefix}ping`)) {
-        await message.channel.send('🚀 pong');
+        if (isChannelValid(message.channel)) {
+            // await message.channel.send('🚀 pong'); // general message
+            await message.reply(' pong!'); // specific message - example
+        }
     }
-})
 
-client.login(process.env.DISCORD_TOKEN);
+    if (message.content.startsWith(`${config.prefix}kick`)) { // kick user
+        if (message.member.hasPermission(['KICK_MEMBERS'])) {
+            const member = message.mentions.members.first();
+            if (member) {
+                const kickedMember = await member.kick();
+                return await message.channel.send(`${kickedMember.user.username} has been kicked`);
+            }
+        }
+        return message.reply('You need permissions to do this');
+    }
+
+    if (message.content.startsWith(`${config.prefix}deleteMessages`)) { // delete last messages
+        const messages = await message.channel.fetchMessages();
+        await message.channel.bulkDelete(messages);
+    }
+});
+
+(async () => {
+    await client.login(process.env.DISCORD_TOKEN);
+})();
+
